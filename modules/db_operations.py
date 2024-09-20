a9d2965fc99d96e2032de73214f08676eee4d276
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2024-09-20 08:30:41 -0300

    fix: table atributes fixed

--- modules/db_operations.py
+++ modules/db_operations.py
@@ -1,33 +1,40 @@
-from sqlalchemy import create_engine, Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Text, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
+
 # Classe que define a tabela 'resumes'
 class Resume(Base):
-    __tablename__ = 'resumes'
+    __tablename__ = "resumes"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     full_text = Column(Text, nullable=False)
-    url = Column(String(255), nullable=False)
+    url = Column(String(255), nullable=False, unique=True)
+
 
 def create_db_session(db_type):
     if db_type == "mysql":
-        engine = create_engine('mysql+mysqlconnector://usertest:Senha123!@localhost/tech_recruiter')
+        engine = create_engine(
+            "mysql+mysqlconnector://usertest:Senha123!@localhost/tech_recruiter"
+        )
     elif db_type == "sqlite":
-        engine = create_engine('sqlite:///tech_recruiter.db')
+        engine = create_engine("sqlite:///tech_recruiter.db")
     else:
         raise ValueError("Tipo de banco de dados inválido. Use 'mysql' ou 'sqlite'.")
 
     Session = sessionmaker(bind=engine)
     return Session(), engine
 
+
 def create_table(engine):
     Base.metadata.create_all(engine)
 
+
 def url_exists(session, url):
     return session.query(Resume).filter(Resume.url == url).count() > 0
+
 
 def add_resume(session, full_text, url):
     new_resume = Resume(full_text=full_text, url=url)
